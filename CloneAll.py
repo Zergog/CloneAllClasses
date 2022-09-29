d6d1677865d3714304dd260821f569d747a1b662
Zergog <zevgoldhabergordon@gmail.com>
2022-09-29 12:54:49 -0500

    Added readme details, automatic submodule initiation

--- CloneAll.py
+++ CloneAll.py
@@ -2,16 +2,17 @@ import os
 import pandas as pd
 
 input = "cellular-automata-assignment-grades-1664435504.csv"
+path = "../"
 df = pd.read_csv(input)
 urls = df.loc[:, ["student_repository_url"]].values.tolist()
 assignment_name = df.loc[0, ["assignment_name"]].values.tolist()[0]
 assignment_name = assignment_name.replace(" ", "")
 print(assignment_name)
-os.system(f"mkdir ../{assignment_name}")
+os.system(f"mkdir {path}{assignment_name}")
 os.chdir(f"../{assignment_name}")
 os.system("pwd")
 print(assignment_name)
 for url in urls:
     url = url[0]
     os.system(f"echo 'Working on {url}'")
-    os.system(f"git clone {url};")
+    os.system(f"git clone --recurse-submodules {url};")
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CloneAllClasses
+Process:
+
+(Step 0. Ensure your SSH key is set up to not require a password with each use.)
+
+Step 1. Download the grade csv from your Github Classroom, as seen below, and put it into your code folder
+
+![alt text](./DownloadGrades.png)
+
+Step 2. Input the name of your csv into the input variable in CloneAll.py
+
+Step 3. Input the path you want to save the repos to into the path variable in CloneAll.py
+
+Step 4. Run CloneAll.py
